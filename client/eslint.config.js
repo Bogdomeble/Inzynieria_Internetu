7216a19ffd3247f6d392660d81026c0b6588374a
EverNever14u <klew200420042004@gmail.com>
2025-11-11 20:44:47 +0100

    eslint fix
    wywalenie 2 wersji eslinta teraz jest tylko jedna globalna

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,23 +1,49 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+// client/eslint.config.js
+
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactRefreshPlugin from 'eslint-plugin-react-refresh';
+import reactPlugin from 'eslint-plugin-react';
+
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+            'react-hooks': reactHooksPlugin,
+            'react-refresh': reactRefreshPlugin,
+            'react': reactPlugin, // Dodaj główny plugin Reacta
+        },
+
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommendedTypeChecked,
+        ],
+
+        rules: {
+            ...reactHooksPlugin.configs.recommended.rules,
+            'react/react-in-jsx-scope': 'off',
+            'react-refresh/only-export-components': 'warn',
+        },
+
+        languageOptions: {
+            ecmaVersion: 2020,
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+            },
+            parser: tseslint.parser,
+            parserOptions: {
+                project: './tsconfig.json',
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
     },
-  },
-])
+]);
