bb90ea2f8f77dcd4e02f3255b5a9ab76c9108c7e
Bogdomeble <bartbod4@gmail.com>
2026-01-20 19:40:19 +0100

    added file compression

--- server/src/uploads/uploads.controller.ts
+++ server/src/uploads/uploads.controller.ts
@@ -7,27 +7,20 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import { extname } from 'path';
+import { memoryStorage } from 'multer'; 
+import { join } from 'path';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-
+import sharp from 'sharp'; 
 @Controller('upload')
 export class UploadsController {
+  
   @Post()
   @UseGuards(JwtAuthGuard)
   @UseInterceptors(
     FileInterceptor('file', {
-      storage: diskStorage({
-        destination: './uploads',
-        filename: (req, file, callback) => {
-          const uniqueSuffix =
-            Date.now() + '-' + Math.round(Math.random() * 1e9);
-          const ext = extname(file.originalname);
-          callback(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
-        },
-      }),
+      storage: memoryStorage(), // Plik trafia do RAM
       limits: {
-        fileSize: 5 * 1024 * 1024, // Limit 5MB
+        fileSize: 5 * 1024 * 1024, // Przyjmujemy nawet 5MB na wejściu
       },
       fileFilter: (req, file, callback) => {
         if (!file.mimetype.match(/\/(jpg|jpeg|png|gif|webp)$/)) {
@@ -40,12 +33,35 @@ export class UploadsController {
       },
     }),
   )
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
     if (!file) {
       throw new BadRequestException('File is not provided');
     }
-    return {
-      url: `http://localhost:3001/uploads/${file.filename}`,
-    };
+
+    // Generujemy nazwę pliku z rozszerzeniem .webp
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const filename = `image-${uniqueSuffix}.webp`;
+    
+    // (ServeStaticModule szuka plików)
+    const filePath = join(process.cwd(), 'uploads', filename);
+
+    try {
+      // SHARP do kompresji
+      await sharp(file.buffer)
+        .resize({ 
+            width: 1200, //  max 1200px 
+            withoutEnlargement: true // Nie powiększamy małych obrazków
+        })
+        .webp({ quality: 80 }) // Kompresja do WebP (80% jakości)
+        .toFile(filePath); 
+
+      //  URL statyczny 
+      return {
+        url: `http://localhost:3001/uploads/${filename}`,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new BadRequestException('Error processing image');
+    }
   }
 }
