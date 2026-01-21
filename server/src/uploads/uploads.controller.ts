51b1727d8126a20db6adf02ce3d93e1f35486604
EverNever14u <klew200420042004@gmail.com>
2026-01-21 15:45:46 +0100

    clean up

--- server/src/uploads/uploads.controller.ts
+++ server/src/uploads/uploads.controller.ts
@@ -7,13 +7,12 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { memoryStorage } from 'multer'; 
+import { memoryStorage } from 'multer';
 import { join } from 'path';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import sharp from 'sharp'; 
+import sharp from 'sharp';
 @Controller('upload')
 export class UploadsController {
-  
   @Post()
   @UseGuards(JwtAuthGuard)
   @UseInterceptors(
@@ -41,21 +40,21 @@ export class UploadsController {
     // Generujemy nazwę pliku z rozszerzeniem .webp
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
     const filename = `image-${uniqueSuffix}.webp`;
-    
+
     // (ServeStaticModule szuka plików)
     const filePath = join(process.cwd(), 'uploads', filename);
 
     try {
       // SHARP do kompresji
       await sharp(file.buffer)
-        .resize({ 
-            width: 1200, //  max 1200px 
-            withoutEnlargement: true // Nie powiększamy małych obrazków
+        .resize({
+          width: 1200, //  max 1200px
+          withoutEnlargement: true, // Nie powiększamy małych obrazków
         })
         .webp({ quality: 80 }) // Kompresja do WebP (80% jakości)
-        .toFile(filePath); 
+        .toFile(filePath);
 
-      //  URL statyczny 
+      //  URL statyczny
       return {
         url: `http://localhost:3001/uploads/${filename}`,
       };
